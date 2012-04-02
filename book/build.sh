24bd157417d02056e9a4d879621c6c4c9dc7b488
Luis Pedro Coelho <luis@luispedro.org>
2012-04-02 20:06:51 +0100

    BLD Basic build infrastructure

---
+++ book/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+function build() {
+    input=$1
+    mkdir -p .$input.tex_files
+    cd .$input.tex_files
+    TEXINPUTS=..:../bioinfo/:.:../../images/:../../figures/: xelatex $input
+    BSTINPUTS=:..:../bioinfo/: BIBINPUTS=:..:.: bibtex $input
+    cp $input.pdf ..
+    cd ..
+}
+build libertarian-welfare
